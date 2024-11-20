ac820a2a4e5f360bb52dbf50047498dc1253e428
Kwenza-Maseko <kwenzaelson80@gmail.com>
2024-11-20 20:17:07 +0200

    routeed

--- app/api/allTask/[id]/route.ts
+++ app/api/allTask/[id]/route.ts
@@ -3,13 +3,13 @@ import { connectToDB } from "@/lib/mongodb/mongoose";
 import { NextResponse } from "next/server";
 
 // No need for interface, just destructure params directly in the context argument
-export async function GET(request: Request, { params }: { params: { taskId: string } }) {
-    const { taskId } = params; // Extract the taskId from params directly
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    const { id } = params; // Extract the taskId from params directly
 
     try {
         await connectToDB(); // Ensure DB connection is made
 
-        const task = await Task.findOne({ _id: taskId }).sort({ createdAt: -1 }).exec();
+        const task = await Task.findOne({ _id: id }).sort({ createdAt: -1 }).exec();
 
         if (!task) {
             return new NextResponse("Task not found", { status: 404 });
