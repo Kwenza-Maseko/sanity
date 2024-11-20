9de5a488e30928804fdf02cac5afe51ec84bc725
Kwenza-Maseko <kwenzaelson80@gmail.com>
2024-11-20 18:53:48 +0200

    task

--- app/api/allTask/[taskId]/route.ts
+++ app/api/allTask/[taskId]/route.ts
@@ -2,18 +2,10 @@ import Task from "@/lib/models/Tasks";
 import { connectToDB } from "@/lib/mongodb/mongoose";
 import { NextResponse } from "next/server";
 
-interface Params {
-    params: {
-        taskId: string;
-    };
-  }
-  
-  export async function GET(request: Request, context: Params) {
-        // Await params before using them
-    
-    try {
-        const { taskId } = await context.params;
+export async function GET(request: Request, context: { params: { taskId: string } }) {
+    const { taskId } = context.params;
 
+    try {
         await connectToDB();
 
         const task = await Task.findOne({ _id: taskId }).sort({ createdAt: -1 }).exec();
